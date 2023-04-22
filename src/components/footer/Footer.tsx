2baf34bae34aecbd9d7f7d27c18cc80d754a54b2
Huining Li <huining.li@fsoft.net.au>
2023-04-23 07:41:51 +1000

    update footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Icon } from '@nebular-react/core';
 import useStyles from './Footer.styles';
 
 export function Footer() {
@@ -6,19 +7,21 @@ export function Footer() {
   return (
     <div className={cx(classes.root)}>
       <span className="created-by">
-        Created with ♥ by{' '}
+        Created with{' '}
         <b>
-          <a href="https://akveo.page.link/8V2f" target="_blank" rel="noreferrer">
-            Akveo
+          <a href="https://github.com/hirol888/nebular-react" target="_blank" rel="noreferrer">
+            Nebular React
           </a>
         </b>{' '}
-        2019
+        2023
       </span>
       <div className="socials">
-        <a href="#" target="_blank" className="ion ion-social-github" />
-        <a href="#" target="_blank" className="ion ion-social-facebook" />
-        <a href="#" target="_blank" className="ion ion-social-twitter" />
-        <a href="#" target="_blank" className="ion ion-social-linkedin" />
+        <a href="https://github.com/hirol888/nebular-react-demo" target="_blank" rel="noreferrer">
+          <Icon icon="logo-github" pack="ion" />
+        </a>
+        <a href="https://www.linkedin.com/in/huining-li-2b05ab26/" target="_blank" rel="noreferrer">
+          <Icon icon="logo-linkedin" pack="ion" />
+        </a>
       </div>
     </div>
   );
